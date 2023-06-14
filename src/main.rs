011cdb199c90688cd07855b25608a96b553996f6
LetsMelon <domi.m@outlook.com>
2023-06-14 14:31:40 +0200

    add a short usage note to main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use std::env;
 use std::path::Path;
+use std::process::exit;
 
 use tsql::parse_file;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let tsql_path = Path::new(args.last().unwrap());
 
-    let parsed = parse_file(tsql_path).unwrap();
-    println!("{:?}", parsed);
+    if args.len() < 2 {
+        println!("usage:");
+        println!("\ttsql PATH_TO_TSQL_FILE");
+        exit(1);
+    }
+
+    match parse_file(Path::new(&args[1])) {
+        Ok(parsed) => {
+            for (_, table) in parsed {
+                println!("{table:?}");
+            }
+        }
+        Err(err) => {
+            println!("encountered an error");
+            println!("{err:?}");
+            exit(1);
+        }
+    }
 }
