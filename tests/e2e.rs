77de983db3a0552178374f574c91a958ab4d219a
LetsMelon <domi.m@outlook.com>
2023-06-15 11:32:29 +0200

    fix tests

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -2,7 +2,7 @@ use std::ffi::OsStr;
 use std::path::Path;
 
 use tsql::parse_file;
-use tsql::types::RawDataType;
+use tsql::types::DataType;
 
 #[test]
 fn e2e_parse_all_files() {
@@ -41,53 +41,41 @@ fn all_types() {
 
     println!("{table:?}");
 
+    assert_eq!(table.get_field("lorem").unwrap().datatype(), &DataType::Int);
     assert_eq!(
-        table.fields.get("lorem").unwrap().raw_datatype,
-        RawDataType::Int
+        table.get_field("ipsum").unwrap().datatype(),
+        &DataType::Bool
     );
     assert_eq!(
-        table.fields.get("ipsum").unwrap().raw_datatype,
-        RawDataType::Bool
+        table.get_field("dolor").unwrap().datatype(),
+        &DataType::BigInt
+    );
+    assert_eq!(table.get_field("sit").unwrap().datatype(), &DataType::Date);
+    assert_eq!(
+        table.get_field("amet").unwrap().datatype(),
+        &DataType::DateTime
     );
     assert_eq!(
-        table.fields.get("dolor").unwrap().raw_datatype,
-        RawDataType::BigInt
+        table.get_field("consetetur").unwrap().datatype(),
+        &DataType::Time
     );
     assert_eq!(
-        table.fields.get("sit").unwrap().raw_datatype,
-        RawDataType::Date
+        table.get_field("elitr").unwrap().datatype(),
+        &DataType::Double
+    );
+    assert_eq!(table.get_field("sed").unwrap().datatype(), &DataType::Float);
+    assert_eq!(table.get_field("diam").unwrap().datatype(), &DataType::Uuid);
+    assert_eq!(
+        table.get_field("nonumy").unwrap().datatype(),
+        &DataType::VarChar(16000)
     );
     assert_eq!(
-        table.fields.get("amet").unwrap().raw_datatype,
-        RawDataType::DateTime
+        table.get_field("eiromod").unwrap().datatype(),
+        &DataType::Char(200)
     );
     assert_eq!(
-        table.fields.get("consetetur").unwrap().raw_datatype,
-        RawDataType::Time
-    );
-    assert_eq!(
-        table.fields.get("elitr").unwrap().raw_datatype,
-        RawDataType::Double
-    );
-    assert_eq!(
-        table.fields.get("sed").unwrap().raw_datatype,
-        RawDataType::Float
-    );
-    assert_eq!(
-        table.fields.get("diam").unwrap().raw_datatype,
-        RawDataType::Uuid
-    );
-    assert_eq!(
-        table.fields.get("nonumy").unwrap().raw_datatype,
-        RawDataType::VarChar(16000)
-    );
-    assert_eq!(
-        table.fields.get("eiromod").unwrap().raw_datatype,
-        RawDataType::Char(200)
-    );
-    assert_eq!(
-        table.fields.get("labore").unwrap().raw_datatype,
-        RawDataType::Text(1024)
+        table.get_field("labore").unwrap().datatype(),
+        &DataType::Text(1024)
     );
 }
 
@@ -103,10 +91,10 @@ fn parse_pks() {
     let all_table = tables.get("Human");
     assert!(all_table.is_some());
     let table = all_table.unwrap();
-    assert_eq!(table.extra.primary_key, ["id"]);
+    assert_eq!(table.primary_keys(), &vec!["id"]);
 
     let all_table = tables.get("Termin");
     assert!(all_table.is_some());
     let table = all_table.unwrap();
-    assert_eq!(table.extra.primary_key, ["start", "end"]);
+    assert_eq!(table.primary_keys(), &vec!["start", "end"]);
 }
