aa250ee2b2cd03f74bd63fd6fc7b19d8e3281fc0
LetsMelon <domi.m@outlook.com>
2023-06-15 16:41:18 +0200

    use pico-args to parse arguments for binary

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,60 @@
-use std::env;
 use std::fs::File;
-use std::path::Path;
+use std::path::PathBuf;
 use std::process::exit;
 
 use tsql::{parse_file, TransformSQL};
 
+const HELP: &str = "\
+tsql
+
+USAGE:
+    tsql [INPUT] [OUTPUT]
+
+FLAGS:
+  -h, --help            Prints help information
+";
+
+#[derive(Debug)]
+struct AppArgs {
+    tsql_path: PathBuf,
+    out_path: PathBuf,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = match parse_args() {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Error: {}.", e);
+            exit(1);
+        }
+    };
 
-    if args.len() < 3 {
-        println!("usage:");
-        println!("\ttsql PATH_TO_TSQL_FILE OUT_FILE");
-        exit(1);
-    }
+    let tables = parse_file(&args.tsql_path).unwrap();
 
-    let tables = parse_file(Path::new(&args[1])).unwrap();
-
-    let mut file = File::create(Path::new(&args[2])).unwrap();
+    let mut file = File::create(&args.out_path).unwrap();
     for (_, table) in tables {
         table.transform(&mut file).unwrap();
     }
+}
+
+fn parse_args() -> Result<AppArgs, pico_args::Error> {
+    let mut pargs = pico_args::Arguments::from_env();
+
+    if pargs.contains(["-h", "--help"]) {
+        print!("{}", HELP);
+        std::process::exit(0);
+    }
+
+    let args = AppArgs {
+        // Parses a required free-standing/positional argument.
+        tsql_path: pargs.free_from_str()?,
+        out_path: pargs.free_from_str()?,
+    };
+
+    let remaining = pargs.finish();
+    if !remaining.is_empty() {
+        eprintln!("Warning: unused arguments left: {:?}.", remaining);
+    }
+
+    Ok(args)
 }
