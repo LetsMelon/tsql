e59c7bcdc5d5dd247e4e7739f33746b75a88d8f1
LetsMelon <domi.m@outlook.com>
2023-06-19 13:41:32 +0200

    use jemallocator to print malloc stats

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,8 +1,27 @@
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 use std::ffi::OsStr;
 use std::path::Path;
 
+use libc::{c_char, c_void};
 use tsql::parse_file;
 use tsql::types::DataType;
+
+extern "C" fn write_cb(_: *mut c_void, message: *const c_char) {
+    print!(
+        "{}",
+        String::from_utf8_lossy(unsafe {
+            std::ffi::CStr::from_ptr(message as *const i8).to_bytes()
+        })
+    );
+}
+
+fn mem_print() {
+    unsafe {
+        jemalloc_sys::malloc_stats_print(Some(write_cb), std::ptr::null_mut(), std::ptr::null())
+    }
+}
 
 #[test]
 fn e2e_parse_all_files() {
@@ -24,8 +43,12 @@ fn e2e_parse_all_files() {
 
     for path in paths {
         println!("path: {:?}", path);
+        mem_print();
+
         let out = parse_file(path);
         assert!(out.is_ok());
+
+        mem_print();
     }
 }
 
