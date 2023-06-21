c22a5c101e7d07a2b81d9303ed8923ddd4cfebe2
LetsMelon <domi.m@outlook.com>
2023-06-21 16:42:48 +0200

    add todo

--- build.rs
+++ build.rs
@@ -1,13 +1,21 @@
 use std::process::Command;
-fn main() {
-    let git_hash = match Command::new("git")
-        .args(&["rev-parse", "HEAD"])
-        .output()
-        .map(|output| String::from_utf8(output.stdout))
-    {
-        Ok(Ok(hash)) => hash,
-        _ => "UNKNOWN_GIT_HASH".to_string(),
-    };
 
+fn unwrap_nested_result<T, X, Y>(input: Result<Result<T, X>, Y>, default: T) -> T {
+    match input {
+        Ok(Ok(value)) => value,
+        _ => default,
+    }
+}
+
+fn main() {
+    let git_hash = unwrap_nested_result(
+        Command::new("git")
+            .args(&["rev-parse", "HEAD"])
+            .output()
+            .map(|output| String::from_utf8(output.stdout)),
+        "UNKNOWN_GIT_COMMIT".to_string(),
+    );
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+
+    // TODO add latest tag to variables
 }
