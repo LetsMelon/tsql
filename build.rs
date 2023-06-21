c9d211bc00c4a932516b0a5c6c0808c1e963b99a
LetsMelon <domi.m@outlook.com>
2023-06-21 16:34:08 +0200

    add warning and commit hash when the executable has been build to generated file

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+fn main() {
+    // note: add error checking yourself.
+    let git_hash = match Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .output()
+        .map(|output| String::from_utf8(output.stdout))
+    {
+        Ok(Ok(hash)) => hash,
+        _ => "UNKNOWN_GIT_HASH".to_string(),
+    };
+
+    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+}
--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,22 @@ fn main() {
     let tables = parse_file(&args.tsql_path).unwrap();
 
     let mut file = BufWriter::new(File::create(&args.out_path).unwrap());
+
+    let out = (0..32).map(|_| "=").collect::<Vec<_>>().join("");
+    writeln!(file, "-- {}", &out).unwrap();
+    writeln!(
+        file,
+        "-- Warning! This file has been generated with tsql. Keep in mind that manuel changes will be overridden."
+    )
+    .unwrap();
+    writeln!(
+        file,
+        "-- Executable tsql build with git commit {:?}",
+        env!("GIT_HASH")
+    )
+    .unwrap();
+    writeln!(file, "-- {}", &out).unwrap();
+
     for (_, table) in tables {
         table.transform(&mut file).unwrap();
     }
