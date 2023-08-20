32a0e8cc1af2969a0b565876aa1a37606fda2e5a
LetsMelon <domi.m@outlook.com>
2023-08-20 02:50:45 +0200

    added some tests in 'lib/src/generate.rs'

--- lib/src/generate.rs
+++ lib/src/generate.rs
@@ -42,3 +42,26 @@ pub fn generate_table(counter: usize, fields_per_table: usize) -> Table {
         TableExtra::new_with_pk(vec![first_field_for_pk]),
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::generate::{hash_number, hash_number_and_stringify, u8s_to_string};
+
+    #[test]
+    fn hash_number_just_works() {
+        assert_eq!(hash_number(1), hash_number(1));
+        assert_ne!(hash_number(1), hash_number(2));
+    }
+
+    #[test]
+    fn u8s_to_string_just_works() {
+        assert_eq!(u8s_to_string(&[0, 1, 2, 3, 4]), "ABCDE".to_string());
+        assert_eq!(u8s_to_string(&[24, 25, 26, 27, 28]), "ABCDE".to_string());
+    }
+
+    #[test]
+    fn hash_number_and_stringify_just_works() {
+        assert_eq!(hash_number_and_stringify(1), hash_number_and_stringify(1));
+        assert_ne!(hash_number_and_stringify(1), hash_number_and_stringify(2));
+    }
+}
